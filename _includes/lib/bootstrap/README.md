3ae21ebdae59382c11476313e64393eadadc4ac1
Lucas Larroche <lucas@larroche.com>
2021-07-03 14:35:09 +0700

    Update Pico (v.1.3.0) and Bootstrap (v5.0.2)

--- _includes/lib/bootstrap/README.md
+++ _includes/lib/bootstrap/README.md
@@ -1,3 +1,3 @@
 # Bootstrap
-- Version `4.5.2`
-- Source code: https://github.com/twbs/bootstrap/releases/tag/v4.5.2
+- Version `5.0.2`
+- Source code: https://github.com/twbs/bootstrap/releases/tag/v5.0.2
--- google-amp/index.php
+++ google-amp/index.php
@@ -3,7 +3,7 @@
 
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Google Amp example • Pico.css</title>
     <meta name="description" content="A simple layout for Google Amp, with inlined CSS, Fixed nav, Sidebar and Social Share.  Built with Pico CSS.">
     <link rel="shortcut icon" href="https://picocss.com/favicon.ico">
