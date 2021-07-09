2db3dc92b8ab752ecde853f50e82c05d522993b0
Lucas Larroche <lucas@larroche.com>
2021-07-09 18:30:08 +0700

    Files structure

---
+++ js/minimal-theme-switcher.js
@@ -0,0 +1,29 @@
+/*!
+ * Minimal theme switcher
+ *
+ * Pico.css - https://picocss.com
+ * Copyright 2020 - Licensed under MIT
+ */
+
+const themeSwitcher = {
+
+  // Config
+  buttonsTarget: 'a[data-theme-switcher]',
+  buttonAttribute: 'data-theme-switcher',
+  rootAttribute: 'data-theme',
+
+  // Init
+  init() {
+    document.querySelectorAll(this.buttonsTarget).forEach(function(button) {
+      button.addEventListener('click', function(event) {
+      event.preventDefault();
+      document.querySelector('html').setAttribute(
+        this.rootAttribute,
+        event.target.getAttribute(this.buttonAttribute));
+      }.bind(this), false);
+    }.bind(this));
+  }
+}
+
+// Init
+themeSwitcher.init();
