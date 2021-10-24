c37c74242b77834a4ec6c9d117a08786d1d5005b
Lucas Larroche <lucas@larroche.com>
2021-10-24 17:29:33 +0700

    Prettify js

--- js/minimal-theme-switcher.js
+++ js/minimal-theme-switcher.js
@@ -6,24 +6,32 @@
  */
 
 const themeSwitcher = {
-
   // Config
-  buttonsTarget: 'a[data-theme-switcher]',
-  buttonAttribute: 'data-theme-switcher',
-  rootAttribute: 'data-theme',
+  buttonsTarget: "a[data-theme-switcher]",
+  buttonAttribute: "data-theme-switcher",
+  rootAttribute: "data-theme",
 
   // Init
   init() {
-    document.querySelectorAll(this.buttonsTarget).forEach(function(button) {
-      button.addEventListener('click', function(event) {
-      event.preventDefault();
-      document.querySelector('html').setAttribute(
-        this.rootAttribute,
-        event.target.getAttribute(this.buttonAttribute));
-      }.bind(this), false);
-    }.bind(this));
-  }
-}
+    document.querySelectorAll(this.buttonsTarget).forEach(
+      function (button) {
+        button.addEventListener(
+          "click",
+          function (event) {
+            event.preventDefault();
+            document
+              .querySelector("html")
+              .setAttribute(
+                this.rootAttribute,
+                event.target.getAttribute(this.buttonAttribute)
+              );
+          }.bind(this),
+          false
+        );
+      }.bind(this)
+    );
+  },
+};
 
 // Init
 themeSwitcher.init();
