3a7e50263b47ca6a33628acc020beb8c672c6604
Lucas Larroche <lucas@larroche.com>
2022-03-13 21:39:41 +0700

    js: Store color scheme in localStorage

--- js/minimal-theme-switcher.js
+++ js/minimal-theme-switcher.js
@@ -6,30 +6,81 @@
  */
 
 const themeSwitcher = {
+  
   // Config
+  _scheme: "auto",
   buttonsTarget: "a[data-theme-switcher]",
   buttonAttribute: "data-theme-switcher",
   rootAttribute: "data-theme",
+  localStorageKey: "picoPreferedColorScheme",
 
   // Init
   init() {
-    document.querySelectorAll(this.buttonsTarget).forEach(
-      function (button) {
-        button.addEventListener(
-          "click",
-          function (event) {
-            event.preventDefault();
-            document
-              .querySelector("html")
-              .setAttribute(
-                this.rootAttribute,
-                event.target.getAttribute(this.buttonAttribute)
-              );
-          }.bind(this),
-          false
-        );
-      }.bind(this)
-    );
+    this.scheme = this.schemeFromLocalStorage;
+    this.initSwitchers();
+  },
+
+  // Get color scheme from local storage
+  get schemeFromLocalStorage() {
+    if (typeof window.localStorage !== "undefined") {
+      if (window.localStorage.getItem(this.localStorageKey) !== null) {
+        return window.localStorage.getItem(this.localStorageKey);
+      }
+    }
+    return this._scheme;
+  },
+
+  // Prefered color scheme
+  get preferedColorScheme() {
+    return window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
+  },
+
+  // Init switchers
+  initSwitchers() {
+    const buttons = document.querySelectorAll(this.buttonsTarget);
+    buttons.forEach((button) => {
+      button.addEventListener(
+        "click",
+        () => {
+          this.scheme = button.getAttribute(this.buttonAttribute);
+        },
+        false
+      );
+    });
+  },
+
+  // Set scheme
+  set scheme(scheme) {
+    if (scheme == "auto") {
+      this.preferedColorScheme == "dark"
+        ? (this._scheme = "dark")
+        : (this._scheme = "light");
+    } else if (scheme == "dark" || scheme == "light") {
+      this._scheme = scheme;
+    }
+    this.applyScheme();
+    this.schemeToLocalStorage();
+  },
+
+  // Get scheme
+  get scheme() {
+    return this._scheme;
+  },
+
+  // Apply scheme
+  applyScheme() {
+    document
+      .querySelector("html")
+      .setAttribute(this.rootAttribute, this.scheme);
+  },
+
+  // Store scheme to local storage
+  schemeToLocalStorage() {
+    if (typeof window.localStorage !== "undefined") {
+      window.localStorage.setItem(this.localStorageKey, this.scheme);
+    }
   },
 };
 
